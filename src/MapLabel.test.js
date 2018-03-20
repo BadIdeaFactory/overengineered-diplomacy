277303dc605c7faa5740fe8815ac0b82d931999d
Lou Huang <lou@louhuang.com>
2018-03-20 16:20:54 -0400

    Add enzyme, make tests passing

--- src/MapLabel.test.js
+++ src/MapLabel.test.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import MapLabel from './MapLabel'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<MapLabel />, div)
-  ReactDOM.unmountComponentAtNode(div)
+it('does not render if city or country props are not provided', () => {
+  const wrapper = shallow(<MapLabel />)
+  expect(wrapper.find('div').length).toEqual(0)
+})
+
+it('does render if city or country props are provided', () => {
+  const wrapper = shallow(<MapLabel city="foo" country="bar" />)
+  expect(wrapper.find('.label-city').text()).toEqual('foo')
+  expect(wrapper.find('.label-country').text()).toEqual('bar')
+})
+
+it('does not render if only one of the props are provided', () => {
+  const wrapper1 = shallow(<MapLabel city="foo" />)
+  expect(wrapper1.find('div').length).toEqual(0)
+
+  const wrapper2 = shallow(<MapLabel country="foo" />)
+  expect(wrapper1.find('div').length).toEqual(0)
 })
