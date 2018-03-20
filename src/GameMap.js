fa0267135d541e0ea36bf2a4461d90d037c2b5d4
Lou Huang <lou@louhuang.com>
2018-03-20 15:20:48 -0400

    React porting: add Tangram back in

--- src/GameMap.js
+++ src/GameMap.js
@@ -1,5 +1,7 @@
+/* global L, Tangram */
 import React from 'react'
-import { Map as Leaflet, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map as Leaflet, TileLayer, ZoomControl, Marker, Popup } from 'react-leaflet'
+import 'leaflet-hash'
 import 'leaflet/dist/leaflet.css'
 import './GameMap.css'
 
@@ -7,7 +9,7 @@ const INITIAL_VIEW = { lng: 18.93, lat: 56.46 }
 const INITIAL_ZOOM = 4
 const BOUNDS_SW = { lng: -11.51, lat: 32.68 }
 const BOUNDS_NE = { lng: 47.38, lat: 71.48 }
-const INITIAL_MAP_OPTIONS = {
+const MAP_OPTIONS = {
   maxBounds: [BOUNDS_SW, BOUNDS_NE],
   maxBoundsViscosity: 1,
   minZoom: 3.5,
@@ -27,79 +29,77 @@ export default class GameMap extends React.Component {
   }
 
   componentDidMount () {
+    const layer = Tangram.leafletLayer({
+      scene: 'scripts/maps/tangram/scene.yaml'
+    })
+
+    const map = this.map.leafletElement
+
+    layer.addTo(map)
+
+    const hash = new L.Hash(map)
+
+    layer.setSelectionEvents({
+      hover: onTangramHover,
+      click: onTangramClick
+    })
     
-    // var map = L.map('map', {
-    //   maxBounds: L.latLngBounds(BOUNDS_SW, BOUNDS_NE),
-    //   maxBoundsViscosity: 1,
-    //   minZoom: 3.5,
-    //   maxZoom: 7,
-    //   attributionControl: false,
-    //   zoomControl: false
-    // }).setView(INITIAL_VIEW, INITIAL_ZOOM)
-    // var zoom = L.control.zoom({ position: 'topright' }).addTo(map)
-    // var hash = new L.Hash(map)
-    // var layer = Tangram.leafletLayer({
-    //   scene: 'scripts/maps/tangram/scene.yaml',
-    // }).addTo(map)
+    var labelEl = document.getElementById('label')
+    var labelCityEl = document.getElementById('label-city')
+    var labelCountryEl = document.getElementById('label-country')
+    var labelShouldStick = false
+    var markers = []
+    var icon = L.icon({
+      iconUrl: 'img/crosshairs.png',
+      iconSize: [24, 24],
+      iconAnchor: [12, 12]
+    })
     
-    // layer.setSelectionEvents({
-    //   hover: onTangramHover,
-    //   click: onTangramClick
-    // })
+    function onTangramHover (e) {
+      if (labelShouldStick === true) return
+      if (e.feature) {
+        labelEl.style.display = 'block'
+        labelCityEl.textContent = e.feature.properties.name
+        labelCountryEl.textContent = e.feature.properties.country
+      } else {
+        labelEl.style.display = 'none'
+        labelCityEl.textContent = ''
+        labelCountryEl.textContent = ''
+      }
+    }
     
-    // var labelEl = document.getElementById('label')
-    // var labelCityEl = document.getElementById('label-city')
-    // var labelCountryEl = document.getElementById('label-country')
-    // var labelShouldStick = false
-    // var markers = []
-    // var icon = L.icon({
-    //   iconUrl: 'img/crosshairs.png',
-    //   iconSize: [24, 24],
-    //   iconAnchor: [12, 12]
-    // })
-    
-    // function onTangramHover (e) {
-    //   if (labelShouldStick === true) return
-    //   if (e.feature) {
-    //     labelEl.style.display = 'block'
-    //     labelCityEl.textContent = e.feature.properties.name
-    //     labelCountryEl.textContent = e.feature.properties.country
-    //   } else {
-    //     labelEl.style.display = 'none'
-    //     labelCityEl.textContent = ''
-    //     labelCountryEl.textContent = ''
-    //   }
-    // }
-    
-    // function onTangramClick (e) {
-    //   for (var i = 0; i < markers.length; i++) {
-    //     markers[i].removeFrom(map)
-    //   }
-    //   if (e.feature) {
-    //     labelEl.style.display = 'block'
-    //     labelCityEl.textContent = e.feature.properties.name
-    //     labelCountryEl.textContent = e.feature.properties.country
-    //     labelShouldStick = true
-    //     var lat = e.feature.properties.coordinates[1]
-    //     var lng = e.feature.properties.coordinates[0]
-    //     var marker = L.marker([lat, lng], { icon: icon }).addTo(map)
-    //     markers.push(marker)
-    //   } else {
-    //     labelEl.style.display = 'none'
-    //     labelCityEl.textContent = ''
-    //     labelCountryEl.textContent = ''
-    //     labelShouldStick = false
-    //   }
-    // }
+    function onTangramClick (e) {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].removeFrom(map)
+      }
+      if (e.feature) {
+        labelEl.style.display = 'block'
+        labelCityEl.textContent = e.feature.properties.name
+        labelCountryEl.textContent = e.feature.properties.country
+        labelShouldStick = true
+        var lat = e.feature.properties.coordinates[1]
+        var lng = e.feature.properties.coordinates[0]
+        var marker = L.marker([lat, lng], { icon: icon }).addTo(map)
+        markers.push(marker)
+      } else {
+        labelEl.style.display = 'none'
+        labelCityEl.textContent = ''
+        labelCountryEl.textContent = ''
+        labelShouldStick = false
+      }
+    }
   }
 
   render () {
     return (
-      <Leaflet center={this.state.position} zoom={this.state.zoom} {...INITIAL_MAP_OPTIONS} className="map">
-        <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <Leaflet
+        center={this.state.position}
+        zoom={this.state.zoom}
+        className="map"
+        ref={(ref) => { this.map = ref }}
+        {...MAP_OPTIONS}
+      >
+        <ZoomControl position="topright" />
       </Leaflet>
     )
   }
